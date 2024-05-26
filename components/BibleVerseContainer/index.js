42774d4ad32160428ba7d0484431f158f70e5d93
lucas <lucasdesouzajacinto@gmail.com>
2024-05-26 12:01:48 -0300

    feat:componente FoundedVerses passa função handleClick para BibleVerseContainer

--- components/BibleVerseContainer/index.js
+++ components/BibleVerseContainer/index.js
@@ -1,26 +1,30 @@
 import {useContext} from "react"
-import {View,Text} from "react-native"
+import {View,Text, Button} from "react-native"
 
 import ShowVerse from "../ShowVerse"
 
 import Books from "../../context/Books"
+import CurrentBook from "../../context/CurrentBook"
 
 export default function BibleVerseContainer ({verse, title, handleClick}){
     const {books} = useContext(Books)
+    const {currentBook} = useContext(CurrentBook)
 
     function findBook(bookid) {
         if (books.allBooks.find((book) => book.bookid === bookid) === undefined) {
           return "livro não encontrado 😢";
         }
         return books.allBooks.find((book) => book.bookid === bookid);
-      }
+    }
 
     return <>
         {verse.verse && 
             <>
-                <View onPressed={() => {
+                <View>
+                    <Button  title="navegar" onPressed={() => {
                     handleClick !== undefined && handleClick(verse);
-                }}>
+                    console.log(currentBook)
+                }}/>
                     {title && <Text>{title}</Text>}
                     <Text>Livro:{findBook(verse.book).name}</Text>
                     <Text>Capitulo:{verse.chapter}</Text>
--- components/FoundedVerses/index.js
+++ components/FoundedVerses/index.js
@@ -1,18 +1,28 @@
 import {useEffect, useContext} from "react"
 
 import Books from "../../context/Books";
+import CurrentBook from "../../context/CurrentBook";
 
 import { ScrollView } from "react-native"
+import { useNavigation } from "@react-navigation/native";
 
 import BibleVerseContainer from "../BibleVerseContainer";
 
 export default function FoundedVerses(){
     const {books} = useContext(Books);
+    const {currentBook,setCurrentBook} = useContext(CurrentBook);
 
-    useEffect(()=>{console.log(books.results)},[books])
+    const navigation = useNavigation()
+    //bug de permanencia do books.results ainda persiste
+    //useEffect(()=>{console.log(books.results)},[books])
+
+    function ChangeChapter(verse){
+        setCurrentBook({...currentBook,book:verse.book,chapter:verse.chapter})
+        navigation.navigate("Bible");
+    }
 
     return <ScrollView>
-            {books.results.map((verse)=> <BibleVerseContainer verse={verse}/>)}
+            {books.results.map((verse)=> <BibleVerseContainer verse={verse} handleClick={ChangeChapter}/>)}
         </ScrollView>
     
 }
